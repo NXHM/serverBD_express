b7afb861924de4f21899a05afdcfba642e9e3970
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-06-21 09:29:59 -0500

    Agregue nuevos endpoints y lo hice dinamico

--- index.js
+++ index.js
@@ -16,11 +16,56 @@ async function verificarConexion(){
     }
     
 }
+app.get("/consultar-usuario", async function(req, res){
+   // const usuarios = await Usuario.findAll(); //select *
+   const {nombre } = req.query;
+   let usuarios;
+   if (nombre==='-1'){
+     usuarios = await Usuario.findAll();
+   }else{
+     usuarios = await Usuario.findAll({
+        where: {
+            nombre
+            
+        }
+    });//select * where
+
+   }
+    
+    
+    res.send(usuarios);
+    //es como un select
+})
+app.get("/modificar-usuario", async function(req, res){
+    // const usuarios = await Usuario.findAll(); //select *
+    const { edad, nombre } = req.query;
+    await Usuario.update({
+         edad             
+         },{
+            where:{
+                nombre
+            }
+         })
+    res.send("Se modifico el usuario")
+     //DELETE FROM "Usuario" WHERE "id" = 1
+ })
+ app.get("/eliminar-usuario", async function(req, res){
+    // const usuarios = await Usuario.findAll(); //select *
+    const { codigo } = req.query;
+     await Usuario.destroy({
+         where: {
+             codigo             
+         }
+     });
+     res.send("Se elimino el usuario")
+ })
 app.get("/insertar-usuario", async function(req,res){
+    const { edad, nombre, codigo } = req.query;
+    //como he nombrado igual la propiedad y el valor, no es necesario que coloque el nombre:nombre...
     const nuevoUsuario= await Usuario.create({
-        nombre: "Juan",
-        codigo: "1234",
-        edad: 20
+        nombre,
+        codigo,
+        edad
     })
     res.send("Usuario creado");
 })
